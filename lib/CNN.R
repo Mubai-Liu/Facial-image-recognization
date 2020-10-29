f1c9ca58ea23e600ba32d663150fa2ef592f5dc5
Mubai Liu <liumubai000@126.com>
2020-10-28 20:47:25 -0400

    Create CNN.R

---
+++ lib/CNN.R
@@ -0,0 +1,46 @@
+library(keras)
+library(tensorflow)
+tensorflow::install_tensorflow()
+tensorflow::tf_config()
+install_tensorflow(gpu=TRUE)
+keras::install_keras(tensorflow="gpu")
+model <- keras_model_sequential() 
+model %>% 
+  #first layer is Convolution Layer
+  layer_conv_1d(filters = 100, kernel_size = 3, activation = "relu",input_shape = c(208,1)) %>%
+  #Second layer is Pooling Layer
+  layer_max_pooling_1d(pool_size = 3) %>%
+  #Third layer is Convolution Layer
+  layer_conv_1d(filters = 100, kernel_size = 3, activation = "relu") %>% 
+  layer_dropout(rate = 0.2) %>% 
+  #Fourth layer is Flatten Layer
+  layer_flatten() %>% 
+  layer_dropout(rate = 0.2) %>%
+  #Fifth layer is Dense Layer
+  layer_dense(units = 100, activation = 'relu') %>% 
+  layer_dropout(rate = 0.2) %>%
+  #Sixth layer is Dense Layer
+  layer_dense(units = 1, activation = 'sigmoid')
+
+model %>% compile(
+  loss=loss_binary_crossentropy,
+  optimizer = optimizer_adam(lr = 0.001),
+  metrics = "binary_accuracy"
+)
+summary(model)
+#dat_train_improved <- dat_train[,-6007]
+#dat_train_improved <- feature_improved(dat_train_improved)
+#x_train <- array(dat_train_improved,dim=c(2400,208,1))
+
+x_train <- array(unlist(dat_train[, -6007]), dim = c(2400, 208, 1))
+y_train <- array(as.numeric(dat_train$label)) - 1
+model %>% fit(
+  x_train, y_train, 
+  epochs = 10, batch_size = 200, 
+  validation_split = 0.2,
+  verbose = 1
+)
+x_test <- array(unlist(dat_test[, -6007]), dim = c(600, 208, 1))
+pred <- predict_classes(model, x_test)
+1-sum(pred != dat_test$label)/length(pred)
+
