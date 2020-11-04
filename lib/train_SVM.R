ee82726b0b4350a3fdae494fc9ba33e27822d425
ZiyuKong <zk2234@columbia.edu>
2020-11-04 10:19:55 -0500

    svm

---
+++ lib/train_SVM.R
@@ -0,0 +1,13 @@
+###########################################################
+### Train a classification model with training features ###
+###########################################################
+svm_train <- function(dat_train, cost = 1, probability = FALSE, ...){
+
+  tm.train = system.time(
+    svm_model <- svm(label ~ ., data = dat_train,
+                     cost = cost, probability=probability,  
+                     ...) 
+  )
+  return(list(model = svm_model, tm.train))
+}
+
